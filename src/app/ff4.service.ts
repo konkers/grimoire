be06e8635f0e819c165ee3478752e92d0e0970e6
Erik Gilling <konkers@konkers.net>
2019-03-08 22:08:05 -0800

    Protoype of getting data back from ff4.

--- src/app/ff4.service.ts
+++ src/app/ff4.service.ts
@@ -1,18 +1,22 @@
 import {Injectable} from '@angular/core';
+import {Ff4Service as ff4s} from '../../crate/pkg/rust_grimoire';
 
 @Injectable({providedIn: 'root'})
 export class Ff4Service {
   module: any;
+  ff4: ff4s;
 
   constructor() {
-import('../../crate/pkg').then(m => {console.log(m); m.run(); this.module = m;
-});
+import('../../crate/pkg').then(m => {console.log(m); m.run(); this.module = m;});
   }
 
   uploadRom(file: File) {
     let reader = new FileReader()
     reader.onloadend = (e) => {
-      this.module.load_rom(new Uint8Array(reader.result as ArrayBuffer));
+      this.ff4 =
+          this.module.load_rom(new Uint8Array(reader.result as ArrayBuffer));
+      console.log(this.ff4);
+      console.log(this.ff4.get_monsters());
     };
     reader.readAsArrayBuffer(file);
   }
